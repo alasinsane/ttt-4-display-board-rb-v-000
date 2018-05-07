d9148f0e7fc9eced3feedee92560044cf7f4ecc5
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-05-07 12:04:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,17 @@
 def display_board(board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "])
+<<<<<<< HEAD
  puts " #{board[0]} | #{board[1]} | #{board[2]} "
  puts "-----------"
  puts " #{board[3]} | #{board[4]} | #{board[5]} "
  puts "-----------"
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+ puts "#{board[0]} | #{board[1]} | #{board[2]}"
+ puts "-----------"
+ puts "#{board[3]} | #{board[4]} | #{board[5]}"
+ puts "-----------"
+ puts "#{board[6]} | #{board[7]} | #{board[8]}"
+>>>>>>> 7ba2e4dc2d7194c71125ce8ee277348a0fcc4930
 end
 
 
